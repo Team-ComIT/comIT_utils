acd7bea4a561b87ea0659658a726a3848ffb3f4b
kimuichan <leerabi577@gmail.com>
2022-10-24 14:22:58 +0900

    feat(styled-components-theme): global style 세팅

--- packages/styled-components-theme/src/ComitStyleProvider.tsx
+++ packages/styled-components-theme/src/ComitStyleProvider.tsx
@@ -1,7 +1,13 @@
 import { PropsWithChildren } from "react";
 import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./GlobalStyle";
 import { theme } from "./theme";
 
 export const ComitStyleProvider = ({ children }: PropsWithChildren) => {
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      {children}
+    </ThemeProvider>
+  );
 };
--- packages/styled-components-theme/src/GlobalStyle.tsx
+++ packages/styled-components-theme/src/GlobalStyle.tsx
@@ -2,12 +2,10 @@ import { createGlobalStyle } from "styled-components";
 
 const styled = { createGlobalStyle };
 
-export const globalStyle = styled.createGlobalStyle`
+export const GlobalStyle = styled.createGlobalStyle`
   * {
     box-sizing: border-box;
     padding: 0;
     margin: 0;
-    line-height: 1.3;
-    font-family: "Spoqa Han Sans Neo", sans-serif;
   }
 `;
