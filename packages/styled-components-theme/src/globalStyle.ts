da4233190c3d643d948dab309c5947b92d748af6
kimuichan <leerabi577@gmail.com>
2022-10-24 14:08:52 +0900

    fix(styled-components-theme): prettier 적용을 위한 작업

--- packages/styled-components-theme/src/globalStyle.ts
+++ packages/styled-components-theme/src/globalStyle.ts
@@ -1,11 +1,13 @@
 import { createGlobalStyle } from "styled-components";
 
-export const globalStyle = createGlobalStyle`
-* {
-        box-sizing: border-box;
-        padding:  0;
-        margin: 0;
-        line-height: 1.3;
-        font-family: 'Spoqa Han Sans Neo', sans-serif;
-    }
+const styled = { createGlobalStyle };
+
+export const globalStyle = styled.createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+    line-height: 1.3;
+    font-family: "Spoqa Han Sans Neo", sans-serif;
+  }
 `;
