25c6e08fbc05eac3dcdf0437aa7ba352a3441b9b
kimuichan <leerabi577@gmail.com>
2022-11-17 17:25:59 +0900

    feat(styled-components-theme): default theme type 빌드되게 추가

--- packages/styled-components-theme/src/@types/theme.d.ts
+++
@@ -1,7 +0,0 @@
-import "styled-components";
-import { theme } from "../theme";
-
-declare module "styled-components" {
-  type CustomTheme = typeof theme;
-  export interface DefaultTheme extends CustomTheme {}
-}
